b5872a3b2b256f5c0697c74ac2035b705477c94c
edrlgo <emi.lop96@gmail.com>
2017-05-04 15:53:22 +0200

    CSS för en kontaktperson

--- template-parts/about-us.php
+++ template-parts/about-us.php
@@ -12,11 +12,10 @@
 
 </div>
 
-<h3 class="site-about-us-header"> <?php the_field('namn') ?> </h3>
+<h3> <?php the_field('namn') ?> </h3>
 
 <span class="site-about-us-role"> <?php the_field('role') ?> </span>
 
 <a class="site-about-us-link" href="tel:<?php the_field('phone-number'); ?>"> <?php the_field('phone-number'); ?> </a>
 
 <a class="site-about-us-link" href="mailto:<?php the_field('email'); ?>"> <?php the_field('email'); ?> </a>
-
