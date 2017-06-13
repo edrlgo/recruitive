b02d0a7c9fd25789139e4f829140faecf1479029
edrlgo <emi.lop96@gmail.com>
2017-06-13 23:13:41 +0200

    CSS för annonslistan
    Även om annonserna ej är klara, kan det ändå vara bra med ett färdigt
    CSS...

--- page-templates/page-lediga-jobb.php
+++ page-templates/page-lediga-jobb.php
@@ -4,9 +4,32 @@
 
 <?php get_template_part( 'template-parts/page-header', get_post_format() ); ?>
 
-	<div class="the-content flexcenter">
-		<iframe width="100%" height="400px" scrolling="no" src="https://cv-recruitive-se.app.intelliplan.eu/JobAd/GroupList">
-		</iframe>
+	<div class="job-ads-content">
+		<!--<iframe width="100%" height="400px" scrolling="no" src="https://cv-recruitive-se.app.intelliplan.eu/JobAd/GroupList">
+		</iframe>-->
+		<div class="jobadgrouplistitemcontainer">
+			<span class="jobadgrouplistitemsubject">
+				Kundtjänstpersonal till Klarna
+			</span>
+		</div>
+
+		<div class="jobadgrouplistitemcontainer">
+			<span class="jobadgrouplistitemsubject">
+				Kundtjänstpersonal till Klarna
+			</span>
+		</div>
+
+		<div class="jobadgrouplistitemcontainer">
+			<span class="jobadgrouplistitemsubject">
+				Kundtjänstpersonal till Klarna
+			</span>
+		</div>
+
+		<div class="jobadgrouplistitemcontainer">
+			<span class="jobadgrouplistitemsubject">
+				Kundtjänstpersonal till Klarna
+			</span>
+		</div>
 	</div>
 
 <?php
