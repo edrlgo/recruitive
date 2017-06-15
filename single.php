fa59522cb62668a6c2840e6b2d2a592a1ccde1a5
hakanfilip <hakanfilipswahn@gmail.com>
2017-06-15 23:07:18 +0200

    Ändringar i loopen - Related Posts
    Ändrat i loppen så att Related post visas korrekt.

--- single.php
+++ single.php
@@ -26,6 +26,46 @@ get_header(); ?>
 
 		endwhile; // End of the loop.
 		?>
+
+		<div class="padding-50"></div>
+
+		<div class="relatedposts">
+		<?php $orig_post = $post;
+		global $post;
+		$tags = wp_get_post_tags($post->ID);
+		if ($tags) {
+		$tag_ids = array();
+		foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+		$args=array(
+		'tag__in' => $tag_ids,
+		'post__not_in' => array($post->ID),
+		'posts_per_page'=>3, // Number of related posts that will be shown.
+		'caller_get_posts'=>1
+		);
+		$my_query = new wp_query( $args );
+		if( $my_query->have_posts() ) {
+		echo '<h3>Läs även</h3><div class="row">';
+		while( $my_query->have_posts() ) {
+		$my_query->the_post(); 
+		$backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+		?>
+		<div class="col-sm-4 col-md-4 col-lg-4 rl-p" style="background: linear-gradient(rgba(64, 64, 64, 0.5), rgba(64, 64, 64, 0.2)), url('<?php echo $backgroundImg[0]; ?>');"><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"></a>
+		<div class="relatedcontent">
+		<h3><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+		</div>
+		</div>
+
+
+		<? }
+		echo '</div>';
+		}
+		}
+		$post = $orig_post;
+		wp_reset_query(); ?>
+
+		</div>
+
+		<div class="padding-50"></div>
 		
 		</main><!-- #main -->
 	</div><!-- #primary -->
