46e8a2d79b8b08868e30131abb6b813b2cd574af
edrlgo <emi.lop96@gmail.com>
2017-06-15 13:07:00 +0200

    Jobblistan först på startsidan
    Bytt plats på index-header och jobbannonserna på startsidan.

--- page-templates/page-startsida.php
+++ page-templates/page-startsida.php
@@ -5,25 +5,8 @@
 	<div id="primary" class="content-area flexcenter">
 		<div class="site-index-header-content">
 			<div class="row">
-				<div class="col-sm-12 col-md-6">
-					<div id="site-index-header">
-						<h1> <?php the_field('stor-rubrik'); ?> </h1>
-						<p> <?php the_field('stor-content_under'); ?> </p>
-						<div class="row">
-							<div class="col-sm-12 col-md-6 col-lg-6">
-								<a class="btn btn-red btn-startsida" href=" <?php the_field('startsida-link-1'); ?>">
-									<?php the_field('startsida-link-1-text'); ?>
-								</a>
-							</div>
-							<div class="col-sm-12 col-md-6 col-lg-6">
-								<a class="btn btn-red btn-startsida" href=" <?php the_field('startsida-link-2'); ?>">
-									<?php the_field('startsida-link-2-text'); ?>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-6">
+
+				<div class="col-sm-12 col-md-4">
 					<div class="site-index-jobs">
 
 						<h2 class="job-header"> <?php the_field('liten-rubrik'); ?> </h2>
@@ -54,6 +37,28 @@
 		
 					</div>
 				</div>
+
+				<div class="col-sm-12 col-md-8">
+					<?php $bild = get_sub_field('index-bild'); ?>
+					<div id="site-index-header" style="background-image: url(<?php echo $bild['index-bild'] ?>)">
+
+						<h1 class="index-header"> <?php the_field('stor-rubrik'); ?> </h1>
+						<p class="index-text"> <?php the_field('stor-content_under'); ?> </p>
+						<div class="row">
+							<div class="col-sm-6 col-md-6 col-lg-6">
+								<a class="btn btn-red btn-startsida" href=" <?php the_field('startsida-link-1'); ?>">
+									<?php the_field('startsida-link-1-text'); ?>
+								</a>
+							</div>
+							<div class="col-sm-6 col-md-6 col-lg-6">
+								<a class="btn btn-red btn-startsida" href=" <?php the_field('startsida-link-2'); ?>">
+									<?php the_field('startsida-link-2-text'); ?>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>
