4874f65e61ea58a13f7e0d995d3706408b2adf49
edrlgo <emi.lop96@gmail.com>
2017-06-16 14:07:41 +0200

    Hela nyhetsdelen

--- template-parts/news-boxes.php
+++ template-parts/news-boxes.php
@@ -1,50 +1,66 @@
-<div class="container-fluid">
+<div class="site-index-news-content">
+	
+	<h2> Senaste nyheterna </h2>
 
-	<div class="vh-box">
+	<div class="container-fluid">
 
-		<!-- bestäm width med hjälp av vh-sizer klassen -->
-		<div class="vh-sizer col-sm-12 col-md-6 col-lg-6"></div>
+		<div class="vh-box">
 
-		<!-- övre vänstra boxen -->
-		<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
+			<!-- bestäm width med hjälp av vh-sizer klassen -->
+			<div class="vh-sizer col-sm-12 col-md-6 col-lg-6"></div>
 
-			<div class="news-content vh-75" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/IMAG0139.jpg')">
-				<a class="news-link" href="#"></a>
-				<span class="news-header">Det bästa med att bo i en storstad</span>
-			</div>
+			<!-- övre vänstra boxen -->
+			<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
 
-		</div>
-
-		<!-- övre högra boxen -->
-		<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
-
-			<div class="news-content vh-50" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/IMAG0143.jpg')">
-				<a class="news-link" href="#"></a>
-				<span class="news-header">Klarna som arbetsgivare</span>
-			</div>
-
-		</div>
-
-		<!-- nedre högra boxen -->
-		<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
-
-			<div class="news-content vh-75" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/Modify-your-computer-with-custom-computer-cases-2.jpg')">
-				<a class="news-link" href="#"></a>
-				<span class="news-header">VLOGG: Sök jobb utan CV</span>
-			</div>
-
-		</div>
-
-		<!-- nedre vänstra boxen -->
-		<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
-
-			<div class="news-content vh-50" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/1.jpg')">
+				<div class="news-content vh-75" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/IMAG0139.jpg')">
 					<a class="news-link" href="#"></a>
-					<span class="news-header">Personligt brev är inte längre viktigt</span>
+					<span class="news-header">Det bästa med att bo i en storstad</span>
+				</div>
+
+			</div>
+
+			<!-- övre högra boxen -->
+			<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
+
+				<div class="news-content vh-50" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/IMAG0143.jpg')">
+					<a class="news-link" href="#"></a>
+					<span class="news-header">Klarna som arbetsgivare</span>
+				</div>
+
+			</div>
+
+			<!-- nedre högra boxen -->
+			<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
+
+				<div class="news-content vh-75" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/Modify-your-computer-with-custom-computer-cases-2.jpg')">
+					<a class="news-link" href="#"></a>
+					<span class="news-header">VLOGG: Sök jobb utan CV</span>
+				</div>
+
+			</div>
+
+			<!-- nedre vänstra boxen -->
+			<div class="vh-item col-sm-12 col-md-6 col-lg-6 no-m">
+
+				<div class="news-content vh-50" style="background-image: url('http://localhost:8888/recruitive/wp-content/uploads/2017/05/1.jpg')">
+						<a class="news-link" href="#"></a>
+						<span class="news-header">Personligt brev är inte längre viktigt</span>
+				</div>
+
 			</div>
 
 		</div>
 
 	</div>
 
+	<div class="padding-50"></div>
+
+	<div class="flexcenter">
+
+		<a class="btn btn-red" href="/nyheter"> Fler nyheter </a>
+
+	</div>
+
 </div>
+
+
