3ed1ab44c1ebf960c1e6ef3ef98ae1e47777b202
hakanfilip <hakanfilipswahn@gmail.com>
2017-06-22 11:54:49 +0200

    Jobblista container

--- page-templates/page-lediga-jobb.php
+++ page-templates/page-lediga-jobb.php
@@ -4,6 +4,8 @@
 
 <?php get_template_part( 'template-parts/page-header', get_post_format() ); ?>
 
+<div class="container-job-ad">
+
 	<div class="job-ads-content">
 		<!--<iframe width="100%" height="400px" scrolling="no" src="https://cv-recruitive-se.app.intelliplan.eu/JobAd/GroupList">
 		</iframe>-->
@@ -31,6 +33,7 @@
 			</span>
 		</div>
 	</div>
+</div> <!-- container -->
 
 <?php
 get_sidebar();
